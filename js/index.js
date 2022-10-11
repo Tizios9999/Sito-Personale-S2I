0dc064825959ad385949e3e2a4c54ae9f2797b60
Tizios9999 <davide.santonocito@outlook.com>
2022-10-11 13:45:40 +0200

    first working version

--- js/index.js
+++ js/index.js
@@ -1,11 +1,24 @@
-const hamBtn = document.getElementById("ham-btn");
+const hamBtn = document.querySelector(".hamburger-button");
 const sideNav = document.querySelector(".side-nav");
 const mainContent = document.querySelector(".main-content");
 const navTop = document.querySelector(".nav-top");
+const sideNavList = document.getElementById("side-nav-list");
 
 hamBtn.addEventListener("click", function () {
     sideNav.classList.remove("d-none");
+    sideNav.classList.add("temporary-menu");
     mainContent.classList.add("shadowed");
     navTop.classList.add("shadowed");
 })
 
+sideNavList.addEventListener("click", function (e) {
+    if (sideNav.classList.contains("temporary-menu")) {
+        if (e.target.tagName="li") {
+            sideNav.classList.remove("temporary-menu");
+            sideNav.classList.add("d-none");
+            mainContent.classList.remove("shadowed");
+            navTop.classList.remove("shadowed");
+        }
+    } 
+    
+})
